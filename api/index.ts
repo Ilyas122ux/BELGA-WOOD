a739f818e9d7be0737987105872c313022dbf50c
Ilyas122ux <belgawood@gmail.com>
2026-08-18 10:15:18 +0100

    Handle Vercel API initialization failures

--- api/index.ts
+++ api/index.ts
@@ -3,11 +3,30 @@ import { createApp } from "../server/src/app.js";
 import { GoogleSheetsBelgaRepository } from "../server/src/belga/GoogleSheetsBelgaRepository.js";
 import { restoreRewrittenPath } from "./_rewrite.js";
 
-const repository = new GoogleSheetsBelgaRepository();
-const appPromise = repository.initialize().then(() => createApp(repository));
+type App = ReturnType<typeof createApp>;
+let appPromise: Promise<App> | undefined;
+
+function getApp() {
+  appPromise ??= Promise.resolve().then(async () => {
+    const repository = new GoogleSheetsBelgaRepository();
+    await repository.initialize();
+    return createApp(repository);
+  });
+  return appPromise;
+}
 
 export default async function handler(req: Request, res: Response) {
   restoreRewrittenPath(req);
-  const app = await appPromise;
-  return app(req, res);
+  try {
+    const app = await getApp();
+    return app(req, res);
+  } catch (error) {
+    appPromise = undefined;
+    console.error("[vercel-api] initialization failed", error);
+    return res.status(503).json({
+      success: false,
+      data: null,
+      message: "Le service est momentanément indisponible.",
+    });
+  }
 }
